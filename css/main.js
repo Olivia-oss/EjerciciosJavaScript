2c3a0be4f2be6a9763ada350266e01be5359ec33
oly <11.oly.adame@gmail.com>
2020-09-08 12:07:31 -0500

    Eliminacion de la carpeta CSS

--- css/main.js
+++
@@ -1,9 +0,0 @@
-import {Funciones} from "./funciones.js";
-let text = "Hola como estas?";
-
-let fuctions = new Funciones();
-
-fuctions.caracteresTexto(text);
-fuctions.recorteTexto(text,4);
-fuctions.separarTexto(text,' ')
-fuctions.repetirTexto(text,3);
