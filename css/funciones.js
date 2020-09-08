60dd0199626a607497411c5d0b2927b2b0290ca4
oly <11.oly.adame@gmail.com>
2020-09-08 11:50:17 -0500

    Verificacion agregada

--- css/funciones.js
+++ css/funciones.js
@@ -1,26 +1,36 @@
+
+
 export class Funciones{
 
+    caracteresTexto = (text="")=>
+    (!text)
+    ?console.warn("No ingresos texto")
+    :console.log("Numeros de caracteres:",text.length);
+    
+    recorteTexto = (text="",numeroCaracteres)=>
+        (!isNaN(text))
+        ?console.warn("No ingresos texto")
+        :(numeroCaracteres != undefined)
+            ?console.log("Texto selecionado: "+ text.substring(0,numeroCaracteres))
+            :console.warn("No ingreso longitud");
+    
+    
+    
+    separarTexto = (text="",separar = undefined)=>
+        (text.length <= 1 )
+        ?console.warn("No ingresos texto")
+        :(separar != undefined)
+            ?console.log("Texto separado: "+  text.split(separar))
+            :console.warn("No ingreso separador"); 
 
-
-    caracteresTexto(text){
-        return text.length;
-    }
-    
-    
-    recorteTexto(text,numeroCaracteres){
-        return text.substring(0,numeroCaracteres);
-    }
     
     
     
-    separarTexto(text,separar){
-        const array = text.split(separar);
-        return array;
-    }
+    repetirTexto = (text="", numeroVeces=undefined)=>
+        (!isNaN(text))
+        ?console.warn("No ingresos texto")
+        :(numeroVeces != undefined)
+            ?console.log("Repiticion de texto: "+ text.repeat(numeroVeces))
+            :console.warn("No ingreso numero de veces repetidas"); 
     
-    
-    
-    repetirTexto(text, numeroVeces){
-        return text.repeat(numeroVeces);
-    }
 }
--- css/main.js
+++ css/main.js
@@ -3,7 +3,7 @@ let text = "Hola como estas?";
 
 let fuctions = new Funciones();
 
-console.log("Numero de caracteres: "+fuctions.caracteresTexto(text));
-console.log("Texto selecionado: "+fuctions.recorteTexto(text,4));
-console.log("Texto separado: "+fuctions.separarTexto(text,' '));
-console.log("Repiticion de texto: "+fuctions.repetirTexto(text,3));
+fuctions.caracteresTexto(text);
+fuctions.recorteTexto(text,4);
+fuctions.separarTexto(text,' ')
+fuctions.repetirTexto(text,3);
